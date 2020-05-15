cb85b93cf3fa2b5fbcab5b86b2a28e44ba338eeb
theKoladeAkande <akandekolade@gmail.com>
2020-05-15 11:09:31 +0000

    fix: pandas dependency issue for loading s3
    Defining the proper channel for loading the training files from s3
    pandas can now read training data from s3

--- preprocessor.py
+++ preprocessor.py
@@ -256,15 +256,6 @@ drop_features = ['Fireplaces']
 
 target = 'SalePrice'
 
-
-s3_train_dir = "s3://krypton-data/ml-data/train/train.csv"
-
-s3_test_dir =  "s3://krypton-data/ml-data/test/test.csv"
-
-s3_model_dir = "s3://krypton-data/model/"
-
-s3_output_dir = "s3://krypton-data/output/"
-
 house_price_preprocessing_pipeline = Pipeline(
     [
     ('categorical_nan_imputer',
@@ -286,12 +277,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Sagemaker specific arguments. Defaults are set in the environment variables.
-    parser.add_argument('--output-data-dir', type=str, default=s3_output_dir)
-    parser.add_argument('--model-dir', type=str, default=s3_model_dir)
-    parser.add_argument('--train', type=str, default=s3_train_dir)
-    args = parser.parse_args()
+    parser.add_argument('--model-dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
+    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
+    args, _ = parser.parse_known_args()
+    print('reading data...')
 
-    raw_data = pd.read_csv(args.train)
+    input_files = [os.path.join(args.train, file) for file in os.listdir(args.train)]
+    if not input_files:
+        raise ValueError(('There are no files in {}.\n' +
+                            'This usually indicates that the channel ({}) was incorrectly specified,\n' +
+                            'the data specification in S3 was incorrectly specified or the role specified\n' +
+                            'does not have permission to access the data.').format(training_path, channel_name))
+    if len(input_files) >= 2:
+        raw_data_list = [ pd.read_csv(file, header=None) for file in input_files ]
+        raw_data = pd.concat(raw_data_list)
+    else:
+        raw_data = pd.read_csv(input_files[0])
 
     raw_data_X = raw_data.iloc[:, :-1]
     raw_data_y = raw_data[[target]]
@@ -303,35 +305,35 @@ if __name__ == "__main__":
                 os.path.join(args.model_dir, "model.joblib"))
     print("Saving model....")
 
-    def input_fn(input_data, content_type):
-        if content_type == 'text/csv':
-            raw_data = pd.read_csv(input_data)
+def input_fn(input_data, content_type):
+    if content_type == 'text/csv':
+        raw_data = pd.read_csv(StringIO(input_data))
 
-            return raw_data
+        return raw_data
+    else:
+        raise ValueError('This script only takes csv')
+
+
+def output_fn(prediction, accept):
+
+        if accept == "application/json":
+            instances = []
+            for row in prediction.tolist():
+                instances.append({"features": row})
+
+            json_output = {"instances": instances}
+
+            return worker.Response(json.dumps(json_output), accept, mimetype=accept)
+        elif accept == 'text/csv':
+            return worker.Response(encoders.encode(prediction, accept), accept, mimetype=accept)
         else:
-            raise ValueError('This script only takes csv')
+            raise RuntimeException("{} accept type is not supported by this script.".format(accept))
 
-
-    def output_fn(prediction, accept):
-
-            if accept == "application/json":
-                instances = []
-                for row in prediction.tolist():
-                    instances.append({"features": row})
-
-                json_output = {"instances": instances}
-
-                return worker.Response(json.dumps(json_output), accept, mimetype=accept)
-            elif accept == 'text/csv':
-                return worker.Response(encoders.encode(prediction, accept), accept, mimetype=accept)
-            else:
-                raise RuntimeException("{} accept type is not supported by this script.".format(accept))
-
-    def predict_fn(input_data, model):
+def predict_fn(input_data, model):
         features =  model.transform(input_data)
         return features
 
 
-    def model_fn(model_dir):
-        preprocessor = joblib.load(os.path.join(model_dir, "model.joblib"))
-        return preprocessor
+def model_fn(model_dir):
+    preprocessor = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return preprocessor
